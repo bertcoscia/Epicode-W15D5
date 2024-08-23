93caaf6812af69e90bdc19d2877783fde6338df0
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-23 11:24:54 +0200

    adds class Magazine

---
+++ src/main/java/aalbertocoscia/entities/Magazine.java
@@ -0,0 +1,41 @@
+package aalbertocoscia.entities;
+
+import aalbertocoscia.enums.Periodicity;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+@DiscriminatorValue("Magazine")
+public class Magazine extends WrittenMedium {
+    @Enumerated(EnumType.STRING)
+    private Periodicity periodicity;
+
+    public Magazine() {
+    }
+
+    public Magazine(String title, Periodicity periodicity) {
+        super(title);
+        this.periodicity = periodicity;
+    }
+
+    public Periodicity getPeriodicity() {
+        return periodicity;
+    }
+
+    public void setPeriodicity(Periodicity periodicity) {
+        this.periodicity = periodicity;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazine{" +
+                "periodicity=" + periodicity +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", numPages=" + numPages +
+                '}';
+    }
+}
