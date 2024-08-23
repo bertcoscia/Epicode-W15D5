52c31b34806066d41d4e8399212126b8c33997eb
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-08-23 12:16:32 +0200

    adds UserDAO

--- src/main/java/aalbertocoscia/Application.java
+++ src/main/java/aalbertocoscia/Application.java
@@ -9,5 +9,6 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
     }
 }
---
+++ src/main/java/aalbertocoscia/dao/UserDAO.java
@@ -0,0 +1,39 @@
+package aalbertocoscia.dao;
+
+import aalbertocoscia.entities.User;
+import aalbertocoscia.exceptions.NotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+import java.util.UUID;
+
+public class UserDAO {
+    private final EntityManager em;
+
+    public UserDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(User user) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(user);
+        transaction.commit();
+        System.out.println("User " + user.getCardId() + " successfully saved");
+    }
+
+    public User findUserById(String userId) {
+        User found = em.find(User.class, UUID.fromString(userId));
+        if (found == null) throw new NotFoundException(userId);
+        return found;
+    }
+
+    public void findUserByIdAndDelete(String userId) {
+        User found = findUserById(userId);
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.remove(found);
+        transaction.commit();
+        System.out.println("User " + found.getCardId() + " successfully removed");
+    }
+}
